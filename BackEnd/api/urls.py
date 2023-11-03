78d1000ef589b2464fa13a43f2390a69b13adfaa
Ken Sheridan <kensheridan86@gmail.com>
2023-11-03 14:25:31 +0000

    simplejwt installed for authentication

--- BackEnd/api/urls.py
+++ BackEnd/api/urls.py
@@ -3,9 +3,13 @@ Urls to access the api
 """
 
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 from . import views
 
+
 urlpatterns = [
+    path('users/login/', TokenObtainPairView.as_view(),
+         name="token_obtain_pair"),
     path('', views.get_routes, name="routes"),
     path('users/', views.get_users, name="users"),
     path('users/<str:pk>/', views.get_user, name="user"),
