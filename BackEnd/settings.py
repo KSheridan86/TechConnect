234eac5d3f47956477b99a2ad46b7b78c9578289
Ken Sheridan <kensheridan86@gmail.com>
2023-12-18 16:07:56 +0000

    api endpoint updated

--- BackEnd/settings.py
+++ BackEnd/settings.py
@@ -182,9 +182,8 @@ STATIC_ROOT = BASE_DIR / "frontend/static"
 
 MEDIA_ROOT = 'static/images'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
-    '*',
     "http://localhost:3000",
     'https://techconnect-19b5c62f6900.herokuapp.com',
     'https://www.techconnect.ie',  # Replace with the actual origin of your React app
